efe6c1737191fef9dd37f874e9f4cae74c0d5825
damian08k <damiankierzkowski08@wp.pl>
2021-03-15 01:00:45 +0100

    Replace aria-expanded changing to different function

--- js/script.js
+++ js/script.js
@@ -3,11 +3,19 @@ const menuNavElement = document.querySelector("[data-menu]");
 
 const handleBurgerClick = () => {
     changeOpenCloseBurger();
+    changeAriaExpandedAttr();
     showHideMenu();
 };
 
 const changeOpenCloseBurger = () => {
     burgerButton.classList.toggle("hamburger--open-burger");
+};
+
+const showHideMenu = () => {
+    menuNavElement.classList.toggle("menu--show-menu");
+};
+
+const changeAriaExpandedAttr = () => {
     const ariaExpanded = "aria-expanded";
 
     const ariaExpandedBtnAttr = burgerButton.getAttribute(ariaExpanded);
@@ -17,10 +25,6 @@ const changeOpenCloseBurger = () => {
     } else if (ariaExpandedBtnAttr === "true") {
         burgerButton.setAttribute(ariaExpanded, "false");
     }
-};
-
-const showHideMenu = () => {
-    menuNavElement.classList.toggle("menu--show-menu");
 };
 
 burgerButton.addEventListener("click", handleBurgerClick);
