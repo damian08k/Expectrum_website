cf63779f6f5c5a1e8b891b27415c1dbef9476370
damian08k <damiankierzkowski08@wp.pl>
2021-05-09 18:39:01 +0200

    Fix Issue #17 - remove scroll event from services section

--- js/services.js
+++ js/services.js
@@ -1,17 +1,13 @@
-const addAnimationToSingleService = () => {
-    const serviceLeftColumn = document.querySelector("[data-serviceLeft]");
-    const serviceRightColumn = document.querySelector("[data-serviceRight]");
+const serviceLeftColumn = document.querySelector("[data-serviceLeft]");
+const serviceRightColumn = document.querySelector("[data-serviceRight]");
 
-    window.addEventListener("scroll", () =>
-        addShowColumnClassForCreateAnimation(serviceLeftColumn, serviceRightColumn)
-    );
-};
-
-const addShowColumnClassForCreateAnimation = (serviceLeftColumn, serviceRightColumn) => {
+const addShowColumnClassForCreateAnimation = () => {
     if (window.innerHeight + window.scrollY > serviceLeftColumn.offsetTop + 50) {
         serviceLeftColumn.classList.add("services__left-column--show-column");
         serviceRightColumn.classList.add("services__right-column--show-column");
+
+        window.removeEventListener("scroll", addShowColumnClassForCreateAnimation);
     }
 };
 
-addAnimationToSingleService();
+window.addEventListener("scroll", addShowColumnClassForCreateAnimation);
